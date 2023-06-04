6d6f050545d5e6ed76ce6f40580870605a005521
mgierschdev <mgierschded@gmail.com>
2023-06-04 09:06:36 -0400

    adding components

--- frontend/src/app/_client_components/StartGameButton.tsx
+++ frontend/src/app/_client_components/StartGameButton.tsx
@@ -2,19 +2,32 @@
 
 import React from 'react';
 import {ChessService} from "@/app/_services/ChessService";
+import {ChessGame} from "@/app/_services/model/ChessGame";
 
 // Game Service
 let gameService: ChessService = new ChessService();
+
+
 
 export default function StartGameButton({gameStarted}: any) {
 
     let started = gameStarted;
 
+    async function startGame(): Promise<void> {
+        let response: ChessGame;
+        if (started) {
+            response = await gameService.endGame();
+        }else{
+            response = await gameService.startGame();
+        }
+        started = response.gameStarted;
+    }
+
     return (
         <div>
             {!started ?
-                <button onClick={() => gameService.startGame()}>Start Game</button> :
-                <button onClick={() => gameService.endGame()}>End Game</button>}
+                <button onClick={() => startGame()}>Start Game</button> :
+                <button onClick={() => startGame()}>End Game</button>}
         </div>
     );
 }
